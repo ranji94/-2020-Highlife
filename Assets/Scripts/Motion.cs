7aeb5efe81e1899c9b5339af7aca4595fd694542
ranji94 <ranji@notowany.pl>
2020-04-10 13:38:16 +0200

    [HLF-1] Examaple project with basic moving implemented

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Itronics.Highlife { 
+    public class Motion : MonoBehaviour
+    {
+        public float speed;
+        private Rigidbody rig;
+
+        private void Start() {
+            Camera.main.enabled = true;
+            rig = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate() {
+            float tHmove = Input.GetAxis("Horizontal");
+            float tVmove = Input.GetAxis("Vertical");
+
+            Vector3 tDirection = new Vector3(tHmove, 0, tVmove);
+            tDirection.Normalize();
+
+            rig.velocity = transform.TransformDirection(tDirection) * speed * Time.deltaTime;
+        }
+    }
+}   
