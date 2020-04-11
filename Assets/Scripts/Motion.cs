a0d54e3e503fa7d1374ae8afccb3f4887b9786e6
ranji94 <ranji@notowany.pl>
2020-04-11 12:17:38 +0200

    [HLF-2] Half-Life like movement

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -6,21 +6,73 @@ namespace Com.Itronics.Highlife {
     public class Motion : MonoBehaviour
     {
         public float speed;
+        public float retarder;
+        public float sprintModifier;
+        private float baseFov;
+        private float sprintFovModifier = 1.1f;
+        public Camera normalCam;
         private Rigidbody rig;
 
         private void Start() {
-            Camera.main.enabled = true;
+            baseFov = normalCam.fieldOfView;
             rig = GetComponent<Rigidbody>();
         }
 
         void FixedUpdate() {
             float tHmove = Input.GetAxisRaw("Horizontal");
             float tVmove = Input.GetAxisRaw("Vertical");
+            bool isSprinting = (Input.GetKey(KeyCode.LeftShift) 
+                || Input.GetKey(KeyCode.RightShift)) 
+                && tVmove > 0;
+            bool isJumping = Input.GetKey(KeyCode.Space);
 
             Vector3 tDirection = new Vector3(tHmove, 0, tVmove);
+            Vector3 jump = new Vector3(0, 10, 0);
             tDirection.Normalize();
 
-            rig.velocity = transform.TransformDirection(tDirection) * speed * Time.deltaTime;
+            normalCam.fieldOfView = baseFov * (isSprinting ? sprintFovModifier : 1f);
+
+            float tAdjustSpeed = speed;
+            if (isSprinting)
+            {
+                tAdjustSpeed *= sprintModifier;
+            }
+
+            if (isJumping) {
+                rig.velocity = transform.TransformDirection(jump);
+            }
+
+            if (isPlayerMoving())
+            {
+                if (retarder < 1f) {
+                    retarder += 0.2f;
+                }
+                rig.velocity = transform.TransformDirection(tDirection)
+                    * tAdjustSpeed
+                    * retarder
+                    * Time.deltaTime;
+            }
+            else
+            {
+                if (retarder > 0.02f) {
+                    retarder -= 0.02f;
+                }
+
+                if (retarder < 0.35f)
+                {
+                    rig.velocity = transform.TransformDirection(tDirection)
+                    * tAdjustSpeed
+                    * retarder
+                    * Time.deltaTime;
+                }
+            }
+        }
+
+        private static bool isPlayerMoving() {
+            float tHmove = Input.GetAxisRaw("Horizontal");
+            float tVmove = Input.GetAxisRaw("Vertical");
+
+            return !(tVmove == 0 && tHmove == 0);
         }
     }
 }   
