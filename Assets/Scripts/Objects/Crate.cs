2ad0bdbc795c5c57af12666598eed1d6e1d407f4
ranji94 <ranji@notowany.pl>
2020-04-14 14:09:51 +0200

    [HLF-5] Crowbar and destroyable crates now works

--- Assets/Scripts/Objects/Crate.cs
+++ Assets/Scripts/Objects/Crate.cs
@@ -6,15 +6,28 @@ namespace Com.Itronics.Highlife {
 public class Crate : MonoBehaviour
 {
         public float durability;
+        public AudioSource hitSound;
+        public AudioSource crateDestroySound;
+        public GameObject fracturedCrate;
 
-        public void takeDamage(float amount) {
+        public void takeDamage(float amount)
+        {
             durability -= amount;
-
-            if (durability <= 0) {
-                Debug.Log("CRATE DESTROYED");
+            if (durability <= 0)
+            {
+                GameObject fracturedGameObj = Instantiate(fracturedCrate, transform.position, Quaternion.identity) as GameObject;
+                Rigidbody[] allRigidBodies = fracturedGameObj.GetComponentsInChildren<Rigidbody>();
+                if (allRigidBodies.Length > 0) {
+                    foreach (var body in allRigidBodies) {
+                        body.AddExplosionForce(3500, transform.position, 1);
+                    }
+                }
+                Destroy(this.gameObject);
+                
             }
 
-            Debug.Log("Crate takes hit");
+            print("Enemy took damage. Durablility left:");
+            print(durability);
         }
-}
+    }
 }
--- Assets/Scripts/Weapons/Crowbar.cs
+++ Assets/Scripts/Weapons/Crowbar.cs
@@ -2,45 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Com.Itronics.Highlife { 
-public class Crowbar : MonoBehaviour
-{
-    public Animator anim;
-    public AudioSource missSound;
-    public float attackDamage;
-    public float attackRange;
-
-    void Start()
+namespace Com.Itronics.Highlife {
+    public class Crowbar : MonoBehaviour
     {
+        public Animator anim;
+        public AudioSource missSound;
+        public Camera weaponCam;
+        public float attackDamage;
+        public float attackRange;
+
+        void Start()
+        {
             anim = GetComponent<Animator>();
             missSound.Play(0);
-    }
+        }
 
-    void Update()
-    {
-            if (Input.GetMouseButton(0))
-            {
-                Debug.Log("CLICKED MOUSE");
-                anim.SetBool("Fire", true);
-                
-                AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Base Layer"));
-                if (!info.IsName("Fire"))
+        void Update()
+        {
+            if (Input.GetMouseButton(0) && !isFiring())
+            { 
+                Ray ray = weaponCam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, attackRange))
                 {
-                    anim.SetInteger("AnimationNumber", 1);
+                    if (hit.collider.tag == "Crate")
+                    {
+                        Crate crate = hit.collider.GetComponent<Crate>();
+                        StartCoroutine(takeDamage(0.2f, crate));
+                    }  
+                }
+                else {
+                    anim.SetBool("Fire", true);
                     playCrowbarMissSound();
-                }  
-            }
-    }
+                }
+            } 
+        }
 
         private void FixedUpdate()
         {
-            AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Base Layer"));
-
-            if (info.IsName("Fire"))
+            if (isFiring())
             {
                 anim.SetBool("Fire", false);
-                anim.SetInteger("AnimationNumber", 0);
+                anim.SetBool("Attack", false);
             }
+        }
+
+        private IEnumerator takeDamage(float time, Crate crate)
+        {
+            yield return new WaitForSeconds(time);
+
+            if(!isFiring()) { 
+                crate.takeDamage(attackDamage);
+                playCrowbarHit(crate.hitSound);
+                anim.SetBool("Attack", true);
+            }
+        }
+
+
+        private bool isFiring()
+        {
+            AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(anim.GetLayerIndex("Base Layer"));
+            return info.IsName("Fire") || info.IsName("Attack");
         }
 
         private void playCrowbarMissSound()
@@ -50,6 +73,12 @@ public class Crowbar : MonoBehaviour
                 missSound.volume = 1f;
                 missSound.Play();
             }
+        }
+
+        private void playCrowbarHit(AudioSource hitSound)
+        {
+            hitSound.volume = 1f;
+            hitSound.Play();
         }
     }
 }
